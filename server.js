08ece26e73d584926db5d508832d87e92f4233fe
Brahm V <brahmvanh@gmail.com>
2023-02-14 21:36:42 -0500

    Let the commit-cascade begin. GET path for notes posting 500 response. Changed path string

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/api/notes/', function(req, res) {
+app.get('api/notes/', function(req, res) {
     fs.readFile(__dirname, '/db/db.json', (err, data) => {
         return res.json((JSON.parse(data)));
     });
 });
 
-app.post('/api/notes/', function(req, res) {
+app.post('api/notes/', function(req, res) {
     let newNote = req.body;
 
     fs.readFile(__dirname, 'db/db.json', (err, data) => {
@@ -39,7 +39,7 @@ app.post('/api/notes/', function(req, res) {
 });
 
 
-app.delete('/api/notes/:id', function(req, res) {
+app.delete('api/notes/:id', function(req, res) {
     let delReq = req.params;
     let id = delReq.id;
 

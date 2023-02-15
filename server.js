1806c3cdc976785e6596a4947ea15c5134ec4a2e
Brahm V <brahmvanh@gmail.com>
2023-02-14 22:50:36 -0500

    added to post request parameters.

--- server.js
+++ server.js
@@ -24,18 +24,34 @@ app.get('/*', function (req, res) {
 });
 
 app.get('/api/notes', function(req, res) {
-    fs.readFile(dbFilePath, (err, data) => {
-        return res.json((JSON.parse(data)));
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+       
+       if (err) {
+        return err;
+    }
+        console.log("You've got data", data)
+        res.json(JSON.parse(data));
     });
 });
 
 app.post('/api/notes', function(req, res) {
-    var newNote = req.body;
-
-    fs.readFile(dbFilePath, (err, data) => {
-        var noteDb = JSON.parse(data);
+    
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if(err) {
+            return console.log(err);
+        } 
+        
+        noteDb = JSON.parse(data);
+        var id = notes[notes.length-1].id + 1
+        var newNote = { title: req.body.title, text: req.body.text, id: id };
         noteDb.push(newNote);
-        fs.writeFile(dbFilePath, JSON.stringify(noteDb));
+        fs.writeFile(dbFilePath, JSON.stringify(noteDb), function (err, data) {
+            if (err) {
+                return err;
+            }
+            console.log(noteDb);
+            res.json(noteDb);
+        });
     });
 });
 
@@ -44,8 +60,9 @@ app.delete('api/notes/:id', function(req, res) {
     let delReq = req.params;
     let id = delReq.id;
 
-    fs.readFile(dbFilePath, (err, data) => {
-         let noteDb = JSON.parse(data);
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+         
+        let noteDb = JSON.parse(data);
         
          const filteredNotes = noteDb.filter((element) => element.id !== id);
 
